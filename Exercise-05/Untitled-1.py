b949ddfdd6120ea393adb74d5790a1383bca7ceb
Andre Firmansyah <andrefirmansyah966@gmail.com>
2022-04-17 21:38:59 +0700

    Exercise-06

---
+++ Exercise-05/Untitled-1.py
@@ -0,0 +1,6 @@
+a = int(input("Inputkan nilai a: "))
+b = int(input("Inputkan nilai b: "))
+
+c = a + b
+print("Hasil penjumlahan =",a,"+",b,"=",c)
+print("Hasil penjumlahan %d + %d = %d ="%(a,b,c))
